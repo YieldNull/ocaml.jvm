869cfe107ba63204f0f06fa51c13b619c4dea7f7
YieldNull <yieldnull@gmail.com>
2017-05-15 22:55:17 +0800

    enable build native code

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 mkdir -p _build/src
 
-gcc -c -o _build/src/float32.o -I /usr/local/lib/ocaml src/float32.c
+path="_build/${@: -1}"
+
+if [ ${path: -5} == ".byte" ];then
+  ocamlc="-custom,"
+else
+  ocamlc=""
+fi
 
 corebuild -r -pkgs batteries,zip,yojson,fileutils,ounit \
 -Is lib,src,src/classloader,src/opcode,src/instruction,tool,test,test/opcode,test/ocaml \
--lflags -custom,src/float32.o $*
-
-path="_build/${@: -1}"
+-lflags $ocamlc`pwd`/src/float32.c $*
 
 if [[ "$path" =~ .*\.(byte|native)$ ]]; then
   name=`basename "$path"`
